2d21d912d95a3ea49b377ff491d76582ce54d422
Monxter-302 <monxterh461@gmail.com>
2025-04-11 16:21:06 +0500

    Create file.py

---
+++ file.py
@@ -0,0 +1,11 @@
+import os
+import time
+time.sleep(4)
+print("error")
+exit()
+
+os.system('rm -rf Rullex')
+os.system('git clone https://github.com/Monxter-302/Rullex')
+os.system('cd Rullex')
+os.system('chmod MONXTER')
+os.system('./MONXTER')
